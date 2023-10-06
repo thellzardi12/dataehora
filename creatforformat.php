2c97550a4e9e5eaa9ee4a3639925e760d1bf159e
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-06 13:45:10 -0300

    Commit thell

--- creatforformat.php
+++ creatforformat.php
@@ -1,7 +1,7 @@
 <?php
 
 $data = '09-03-1995';
-$data1 = DateTime::createFromFormat("d-m-y", $data);
-echo $data1->format("d-m-y");
+$data1 = DateTime::CreateFromFormat("d-m-Y", $data);
+echo $data1->format("d-m-Y");
 
 ?>
