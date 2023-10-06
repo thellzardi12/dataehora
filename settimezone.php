8f060eff3ef6cb585c87c236a399a6f4b14ca234
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-06 11:53:34 -0300

    commit thell

--- settimezone.php
+++ settimezone.php
@@ -1,5 +1,5 @@
 <?php
-    $fuso = new DateTimeZone('America/New York');
+    $fuso = new DateTimeZone('America/New_York');
     $data = new Datetime('28-09-2023');
     $data->setTimezone($fuso);
    echo $data->format('d-m-Y H:i:s');
